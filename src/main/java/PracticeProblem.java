ec3de9cfbd27d49cabba26dce868477b9bfc1910
ALEX-L1ANG <343293684@tdsb.ca>
2026-04-10 15:50:39 +0000

    update1

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,41 @@
+/*
+File Name: Problem Solving and Good Programming Style
+Author: Alex Liang
+Date Created: April 10, 2026
+Date Last Modified: April 10, 2026
+*/
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
 
-	public static void q1() {
-		//Write question 1 code here
+	public static String getFirstName(String names) {
+		names = names.trim();
+		return names.substring(0, names.indexOf(" "));
 	}
 
-	public static void q2() {
-		//Write question 2 code here
+	public static String getLastName(String names) {
+		names = names.trim();
+		return names.substring(names.lastIndexOf(" ") + 1);
 	}
 
-	public static void q3() {
-		//Write question 3 code here
+	public static boolean isValidName(String name) {
+		if (!name.contains(" ")) {
+			return false;
+		} else if (name.indexOf(" ") != name.lastIndexOf(" ")) {
+			return false;
+		}
+		else {
+			String firstName = name.substring(0, name.indexOf(" "));
+			String lastName = name.substring(name.indexOf(" ") + 1);
+			if (firstName.length() < 2 || lastName.length() < 2) {
+				return false;
+			}
+			else {
+				return true;
+			}
+		}
 	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
-	}
-
 }
